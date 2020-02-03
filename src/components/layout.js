47be1c09b007f5521c10dc453bec2e39a869ec4a
bcrave <brennycode@gmail.com>
2020-02-03 06:59:29 -0700

    build out header and navbar components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 
+import Header from "./header-navbar/header";
+import Navbar from "./header-navbar/navbar";
+
 class Layout extends Component {
   render() {
-    return;
-    <div className="app">{this.props.children}</div>;
+    return (
+      <div className="layout">
+        {this.props.children}
+        <Header />
+        <Navbar />
+      </div>
+    );
   }
 }
 
