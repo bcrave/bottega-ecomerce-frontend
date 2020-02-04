dcd822933513aa1f180e5caa3cd237c0a423f2b7
bcrave <brennycode@gmail.com>
2020-02-03 17:55:41 -0700

    developed sign up form and modified form button to have short gray case

--- src/components/auth/sign-up.js
+++ src/components/auth/sign-up.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import SignUpForm from "./sign-up-form";
+import PageTitle from "../page-title";
 
 class SignUp extends Component {
+  onSubmit = (fields) => {
+    console.log(fields);
+  };
+
   render() {
-    return <div className="sign-up">sign up</div>;
+    return (
+      <div className="sign-up">
+        <PageTitle className="sign-up__page-title" title="Register" />
+        <SignUpForm onSubmit={this.onSubmit} className="sign-up__form" />
+      </div>
+    );
   }
 }
 
